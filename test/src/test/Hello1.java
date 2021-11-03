04e6d20b135a910409bfda104c5c8807c2f2326f
user <user@DESKTOP-4NMVE1B>
2021-11-03 17:19:21 +0900

    test

---
+++ test/src/test/Hello1.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Hello1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		/*
+		 * i = 10;
+		 * i = "捞件公"
+		 */
+		int i = 10;
+		String str = "捞件公";
+		System.out.println(i);
+		System.out.println(str);
+		// i = "捞件公";
+		int y;
+		y = 20;
+		System.out.println(y);
+	}
+}
